8e66e4f5f0d62b36e7e185303a475a012743908d
davidrey123 <davidrey.maths@gmail.com>
2024-05-17 11:05:14 +0200

    Update main.py
    added code to solve tapas for custom y

--- main.py
+++ main.py
@@ -12,25 +12,23 @@ from src import Leblanc
 net = 'SiouxFalls'
 ins = 'SF_DNDP_10_1'
 
-network = Network.Network(net,"",0.5,1e-0,1e-3)
+network = Network.Network(net,ins,0.5,1e-0,1e-3)
 #network = Network.Network("grid3",1,1,1,1,1)
 print(net,ins)
-#print()
-
-y1 = {ij:1 for ij in network.links2}
-y0 = {ij:0 for ij in network.links2}
-
-y1[network.findLink(network.findNode(11), network.findNode(150))] = 0
-
-#print(network.tapas('SO', y1))
-
-#print(network.tapas('SO', y1))
-
-#for ij in network.links2:
-#    print(ij, ij.x)
+print()
 
 timelimit = 600
 
 leblanc = Leblanc.Leblanc(network, timelimit)
 
 leblanc.BB()
+
+
+'''
+yy = {(11, 15): 1, (15, 11): 1, (12, 14): 1, (14, 12): 1, (10, 19): 1, (19, 10): 1, (2, 13): 0, (13, 2): 0, (2, 12): 0, (12, 2): 0}
+y = {}
+for a in network.links2:
+    y[a] = yy[(a.start.id,a.end.id)]
+print(y)
+print(network.tapas('UE', y))
+'''
