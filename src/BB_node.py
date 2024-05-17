41db61a2c86af1dc590e3b31f612be0e82e816f9
davidrey123 <davidrey.maths@gmail.com>
2024-05-17 11:07:54 +0200

    Update BB_node.py
    Fixed bugs and edited class constructor

--- src/BB_node.py
+++ src/BB_node.py
@@ -1,6 +1,6 @@
 class BB_node:
     
-    def __init__(self, network, id, parent, LB, UB, fixed0, fixed1):
+    def __init__(self, network, id, parent, LB, UB, fixed0, fixed1, solved):
         self.network = network
         self.parent = parent
         self.id = id
@@ -10,20 +10,20 @@ class BB_node:
         self.fixed0 = fixed0 #list of A2 links fixed to 0
         self.fixed1 = fixed1 #list of A2 links fixed to 1
         self.active = True
-        self.solved = False
+        self.solved = solved
         self.score = {}
         self.ybr = None
         
     def check(self):
         status = 'solve'
-        cost = sum(a.cost for a in self.fixed1)
+        cost = sum(a.cost for a in self.network.links2 if a.id in self.fixed1)
         
         if cost > self.network.B:
             status = 'infeasible'
             return status
             
         fixed = self.fixed0 + self.fixed1
-        free = [a for a in self.network.links2 if a not in fixed]
+        free = [a.id for a in self.network.links2 if a.id not in fixed]
         
         if len(free) == 0:
             status = 'fixed'
@@ -31,7 +31,7 @@ class BB_node:
             
         else:
             left = self.network.B - cost
-            mincost = min([a.cost for a in free])
+            mincost = min([a.cost for a in self.network.links2 if a.id in free])
             
             if left < mincost:
                 status = 'stop'
