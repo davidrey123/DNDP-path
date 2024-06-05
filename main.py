30311d069f1e010e8390c3e2dccf705b4d9443ed
davidrey123 <davidrey.maths@gmail.com>
2024-06-05 22:46:13 +0200

    execute BPC but not working yet

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ Created on Wed Mar 27 18:46:55 2024
 from src import Network
 from src import Leblanc
 from src import FS_NETS
+from src import BPC
 
 net = 'SiouxFalls'
-ins = 'SF_DNDP_10_6'
+ins = 'SF_DNDP_10_1'
 
 
+network = Network.Network(net,ins,0.5,1e-0,1e-3)
+print(net,ins)
+bpc = BPC.BPC(network)
+bpc.BB()
+print()
+
+'''
 network = Network.Network(net,ins,0.5,1e-0,1e-3)
 print(net,ins)
 leblanc = Leblanc.Leblanc(network)
 leblanc.BB()
 print()
+'''
 
 network = Network.Network(net,ins,0.5,1e-0,1e-3)
 print(net,ins)
