3160b5e728b76dbea3da11cb14fce7c2a6760624
davidrey123 <davidrey.maths@gmail.com>
2024-06-10 08:51:43 +0200

    Added call to BC (branch and cut) class

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 27 18:46:55 2024
-
-@author: david
-"""
-
 #---modules
 from src import Network
 from src import Leblanc
 from src import FS_NETS
+from src import BC
 from src import BPC
 
 net = 'SiouxFalls'
-ins = 'SF_DNDP_10_1'
+ins = 'SF_DNDP_10_4'
 
 
+network = Network.Network(net,ins,0.5,1e-0,1e-3)
+print(net,ins)
+bc = BC.BC(network)
+bc.BB()
+print()
+
+'''
 network = Network.Network(net,ins,0.5,1e-0,1e-3)
 print(net,ins)
 bpc = BPC.BPC(network)
 bpc.BB()
 print()
-
+'''
 '''
 network = Network.Network(net,ins,0.5,1e-0,1e-3)
 print(net,ins)
@@ -34,15 +35,3 @@ print(net,ins)
 fs_nets = FS_NETS.FS_NETS(network)
 fs_nets.BB()
 print()
-
-
-
-'''
-network = Network.Network(net,ins,0.5,1e-0,1e-3)
-yy = {(7, 16): 0, (16, 7): 0, (19, 22): 1, (22, 19): 1, (11, 15): 1, (15, 11): 1, (9, 11): 0, (11, 9): 0, (13, 14): 0, (14, 13): 1}
-y = {}
-for a in network.links2:
-    y[a] = yy[(a.start.id,a.end.id)]
-print(y)
-print(network.tapas('UE', y))
-'''
