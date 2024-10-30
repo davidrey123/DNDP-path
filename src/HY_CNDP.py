c52f15fafb04d9574b9e4001c95c4d5c14f4e3c9
Michael Levin <michaellevin@infra04-wg012.eduroam.wireless.umn.edu>
2024-10-30 12:01:03 -0500

    polytope working, time is 54s on Braess network

--- src/HY_CNDP.py
+++ src/HY_CNDP.py
@@ -32,7 +32,7 @@ class HY_CNDP:
         time1 = time.time()
         S0, A, b = self.constructS0()
         self.solveSLP(S0, A, b, 1)
-        time1 = time.time()
+        time1 = time.time() - time1
         
         print("time", time1)
     
@@ -61,7 +61,9 @@ class HY_CNDP:
                     best_obj = SLP_obj
                     best_todtv = tdotv
                     best_t1 = t1
-
+                
+            print("best", best_var, best_obj, SLP_obj)
+            
             # if tdotv - t1 <= delta^k, terminate with z^k
             if best_tdotv - best_t1 <= delta_k:
                 return best_obj
@@ -70,6 +72,9 @@ class HY_CNDP:
             # add linear constraints to 
             
     def evaluate(self, vars):
+        for a in self.network.links:
+            a.x = 0
+            
         for r in range(0, len(self.network.origins)):
             for i in range(0, len(self.network.links)):
                 index = r * len(self.network.links) + i
@@ -80,7 +85,7 @@ class HY_CNDP:
         
         t1 = vars[-1]
         
-        tdotv = sum(a.getTravelTimeC(a.x, a.add_cap) * a.x for a in self.network.links)
+        tdotv = sum(a.getTravelTimeC(a.x, a.add_cap, "UE") * a.x for a in self.network.links)
         
         output = tdotv
         return output, tdotv, t1
@@ -96,14 +101,14 @@ class HY_CNDP:
         b_demo = np.array([2.0, 1.0, 0.0, 0.0])
         
         xvarslen = len(self.network.links)*len(self.network.origins) # x per origin
-        xconslen = 2*len(self.network.origins)*len(self.network.nodes) 
-        num_cons = xconslen+len(self.varlinks)+1 # +1 for t_1
+        print(len(self.network.links), len(self.network.origins))
+        
+        xconslen = len(self.network.origins)*len(self.network.nodes) 
+        num_cons = xconslen+len(self.varlinks)+2  # +1 for t_1
         cols = xvarslen + len(self.varlinks) + 1 # 1 for t1 limit
-        #num_cons = len(self.varlinks)+1
-        #cols = len(self.varlinks)+1
         
         
-        rows = num_cons + cols
+        rows = num_cons + cols + xvarslen
         
         # v first
         
@@ -113,17 +118,25 @@ class HY_CNDP:
         print(rows, cols, len(A), len(A[0]))
         
         
+        rowidx = 0
+        
         
         # link flows
+        eps = 1e-4
+        
+        count = 0
+        
         for ri in range(0, len(self.network.origins)):
             r = self.network.origins[ri]
 
             for j in self.network.nodes:
             
-                rowi = 2* (ri*len(self.network.nodes) + (j.id-1))
                 
-                row1 = A[rowi]
-                row2 = A[rowi+1]
+                
+                row1 = A[rowidx]
+                #row2 = A[rowidx+1]
+                
+                
             
                 dem = 0
                 if j.id == r.id:
@@ -131,45 +144,97 @@ class HY_CNDP:
                 elif isinstance(j, type(r)) == True:
                     dem = r.getDemand(j)
 
-                for ij in j.incoming:
-                    varidx = ri*len(self.network.links)+ij.id
-                    row1[varidx] = 1
-                    #row2[varidx] = -1
-                for jk in j.outgoing:
-                    varidx = ri*len(self.network.links)+jk.id 
-                    row1[varidx] = -1
-                    #row2[varidx] = 1
-
-                b[rowi] = dem
-                #b[rowi+1] = -dem
-                break
-            break
                 
+
+                    
+                #print(rowidx, "node", j, "origin", r.id)
+                
+                if dem <= 0:
+                    for ij in j.incoming:
+                        #print("\tin", ij.id, ij)
+                        varidx = ri*len(self.network.links)+ij.id
+                        row1[varidx] = 1
+                        #row2[varidx] = -1
+                    for jk in j.outgoing:
+                        #print("\tout", jk.id, jk)
+                        varidx = ri*len(self.network.links)+jk.id 
+                        row1[varidx] = -1
+                        #row2[varidx] = 1
+                        
+                    b[rowidx] = dem
+                    rowidx += 1
+                elif dem > 0:    
+                    for ij in j.incoming:
+                        print("\tin", ij.id, ij)
+                        varidx = ri*len(self.network.links)+ij.id
+                        row1[varidx] = -1
+                        #row2[varidx] = -1
+                    for jk in j.outgoing:
+                        print("\tout", jk.id, jk)
+                        varidx = ri*len(self.network.links)+jk.id 
+                        row1[varidx] = 1
+                        #row2[varidx] = 1
+                    b[rowidx] = -dem
+                    rowidx += 1
+                
+                #b[rowidx+1] = -dem
+                
+                
+                
+                
+                
+         
         
         # then y
         for i in range(0, len(self.varlinks)):
-            rowi = i + xconslen 
+            
             coli = i + xvarslen
             
-            rowy = A[rowi]
+            rowy = A[rowidx]
             rowy[coli] = 1
-            b[rowi] = self.varlinks[i].C/2
+            b[rowidx] = self.varlinks[i].C/2
+            rowidx += 1
         
         # t_1 <= M
-        A[rows-1][cols-1] = 1
-        b[cols-1] = 1e20
+        A[rowidx][cols-1] = 1
+        b[rowidx] = 1e20
+        
+        rowidx += 1
+        
+        A[rowidx][cols-1] = -1
+        b[rowidx] = 0
+        
+        rowidx += 1
         
         for i in range (0, cols):
-            rowi = num_cons + i
-            A[rowi][i] = -1
+            A[rowidx][i] = -1
+            rowidx += 1
             
         
-
+        # upper bounds
+        
+        for i in range(0, len(self.network.origins)):
+            for j in range(0, len(self.network.links)):
+                
+                #print("check ub", num_cons, cols, i, j)
+                origin = self.network.origins[i]
+                col = i*len(self.network.links)+self.network.links[j].id
+                A[rowidx][col] = 1
+                b[rowidx] = origin.totaldemand
+        
+                rowidx += 1
+               
         # Ax <= b
         p = pc.Polytope(A, b)
         
+        print(A)
+        print(b)
         
+        #print([4000, 4000, 0, 0, 0, 0] in p)
         
-        #print(len(pc.extreme(p)))
+        ext = pc.extreme(p)
+        
+        #for e in ext:
+        #    print(e)
         
         return p, A, b
